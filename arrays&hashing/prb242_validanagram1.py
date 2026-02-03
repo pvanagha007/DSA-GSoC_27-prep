9549c6cc1ccd56667c264072fe02940379c6285d
Anagha P V <anaghavasishta07@gmail.com>
2026-02-03 19:12:22 +0530

    valid anagram technique 1

---
+++ arrays&hashing/prb242_validanagram1.py
@@ -0,0 +1,8 @@
+#using built in library
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if Counter(s)==Counter(t):
+            return True
+        else:
+            return False
